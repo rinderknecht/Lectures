a9218a5e4ff6fc58c36165fe72aacbcece9c3a1e
Christian Rinderknecht <rinderknecht@free.fr>
2026-08-16 11:08:32 +0200

    Upgraded OCaml/Cours

--- OCaml/Cours/a.ml
+++ OCaml/Cours/a.ml
@@ -1,7 +1,5 @@
 type carte = Carte of ordinaire | Joker
 and ordinaire = couleur * figure
 and couleur = Coeur | Carreau | Pique | Trefle
-and figure = As | Roi | Reine | Valet | Simple of int
-;;
-
-fun (x,y) -> match (x,y) with  (Carte z, z) -> true
+and figure = As | Roi | Reine | Valet | Simple of int;;
+fun (x,y) -> match (x,y) with (Carte z, z) -> true;;
